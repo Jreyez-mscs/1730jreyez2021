676a81b6fdb1b57e4ceda7c9731b3e37df881366
Jakoby Reyez <es7723be@go.minnstate.edu>
2021-12-16 22:57:01 -0600

    Partially completed Ex3c

---
+++ jreyez3c/Ex3cCalculations.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jreyez3c
+{
+    public class Ex3cCalculations
+    {
+        public static string Calc0(int index)
+            //        return     arguement type
+        {
+            try
+            {
+                
+                string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+                return days[index - 1];
+            }
+            catch
+            {
+                return "Invalid index";
+            }
+            
+        }
+
+        public static string Calc1(string time)
+        {
+            string[] days = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
+            string[] hours = { "Closed", "10am - 6pm", "10am - 6pm", "10am - 6pm", "10am - 9pm", "10am - 6pm", "8am - 4pm" };
+            //                    0        1            2             3             4              5            6
+
+            time = time.Trim();
+            time = time.ToUpper();
+            for (int i = 0; i < days.Length; i++)
+            {
+                if (time == days[i])
+                {
+                    return hours[i];
+                }
+                
+            }
+            return "Invalid input";
+            
+            
+            
+        }
+
+        public static int Calc2(int[] numbers)
+        {
+            int total = 0;
+            foreach (int i in numbers)
+            {
+                total += i;
+            }
+            return total;
+        }
+        public static double Calc3(double[] numbers, int count)
+        {
+            double total = 0.0;
+            if (count <= 5)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                        total += numbers[i];
+                    
+                }
+                }
+                else
+                {
+                    return 0.0;
+                }
+                return total;
+        }
+        public static double Calc5(double[] index)
+        {
+            return 0.0;
+        }
+
+        public static double[] Calc6(double[] index)
+        {
+            return new double[10];
+        }
+    }
+}
---
+++ jreyez3cUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("jreyez3cUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("jreyez3cUnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2cd840bb-61df-4aac-88ca-9ade3889721b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
